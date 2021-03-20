3a796635d2c93d6aa95ae67f50f3d5901503279a
zayedibnibrahim <zayed.ibn@gmail.com>
2021-03-20 16:00:49 +0600

    first commit

---
+++ rider-data.js
@@ -0,0 +1,22 @@
+[
+    {
+        "id": 1,
+        "veheivle": "Bike",
+        "image": "../src/image/bike.png"
+    },
+    {
+        "id": 2,
+        "veheivle": "Car",
+        "image": "../src/image/car.png"
+    },
+    {
+        "id": 3,
+        "veheivle": "Bus",
+        "image": "../src/image/bus.png"
+    },
+    {
+        "id": 4,
+        "veheivle": "Train",
+        "image": "../src/image/train.png"
+    }
+]
